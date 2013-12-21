06b4406ea0b97eff8cd35dc6e9ac822e7e2715dd
dstoianov <denys.stoianov@gmail.com>
2013-12-21 13:29:39 +0200

    xls writer is ready and works as updater for xls file

--- src/test/java/org/jsoup/test/RentApartmentGrabber.java
+++ src/test/java/org/jsoup/test/RentApartmentGrabber.java
@@ -4,13 +4,20 @@ package org.jsoup.test;
  * Created by User on 12/18/13.
  */
 
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -20,13 +27,13 @@ import java.util.List;
  */
 public class RentApartmentGrabber {
 
-    List<RentDataTO> list = new ArrayList<RentDataTO>();
+    public static List<RentDataTO> list = new ArrayList<RentDataTO>();
 
     @Test
     public void AvisoTest() {
         //String url = "http://forum.od.ua/forumdisplay.php?f=172";
         String url = "http://bit.ly/JD1ADO";
-
+        int before = list.size();
         print("Fetching %s...", url);
         Document doc;
         try {
@@ -34,12 +41,14 @@ public class RentApartmentGrabber {
             Elements adAll = doc.select("#highlight-text");
 
             for (Element adOne : adAll) {
-                if (adOne.select(".price").toString().equals("")) { continue; }
+                if (adOne.select(".price").toString().equals("")) {
+                    continue;
+                }
                 String adText = adOne.text();
                 String adPrice = adOne.select(".price").text().substring(21);
                 String adDate = adOne.select("div > span").text();
                 String adAddress = adOne.select("a > span").text();
-                String adMessage =  adText.substring(adText.indexOf(adAddress) + adAddress.length());
+                String adMessage = adText.substring(adText.indexOf(adAddress) + adAddress.length());
                 String adURL = adOne.select("a").attr("href").toString();
 
                 RentDataTO rentData = new RentDataTO();
@@ -62,15 +71,14 @@ public class RentApartmentGrabber {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        print("Were added to the base %s ads from Aviso", list.size());
-     }
-
+        print("Were added to the base %s ads from Aviso", list.size() - before);
+    }
 
     @Test
     public void SlandoTest() {
 
         String url = "http://bit.ly/1gJhQkV";
-
+        int before = list.size();
         print("Fetching %s...", url);
         Document doc;
         try {
@@ -78,9 +86,11 @@ public class RentApartmentGrabber {
             Elements adAll = doc.select("#offers_table > tbody > tr");
 
             for (Element adOne : adAll) {
-                if (adOne.select("h3").toString().equals("")) { continue; }
+                if (adOne.select("h3").toString().equals("")) {
+                    continue;
+                }
                 String adAddress1 = adOne.select("p.color-9").text();
-                String adAddress =  adAddress1.substring(adAddress1.indexOf("Одесса") + "Одесса".length());
+                String adAddress = adAddress1.substring(adAddress1.indexOf("Одесса") + "Одесса".length());
                 String adMessage = adOne.select("span.fbold").text();
                 String adPrice = adOne.select("strong.c000").text();
                 String adDate = adOne.select("p.margintop10").first().text();
@@ -102,13 +112,60 @@ public class RentApartmentGrabber {
 //                        "\n3) Price: %s " +
 //                        "\n4) Date: %s " +
 //                        "\n5) URL: %s", adAddress, adMessage, adPrice, adDate, adURL);
-                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        print("Were added to the base %s ads from Slando", list.size());
+        print("Were added to the base %s ads from Slando", list.size() - before);
     }
 
+
+    @AfterClass
+    public static void WriteXlsx() {
+        try {
+            FileInputStream file = new FileInputStream(new File("src/test/resources/rent_list.xlsx"));
+            XSSFWorkbook workbook = new XSSFWorkbook(file);
+
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            int lastrow = sheet.getLastRowNum();
+            //Create a blank sheet
+            //XSSFSheet sheet = workbook.createSheet("Rent Data");
+
+//            RentDataTO rentData = new RentDataTO();
+//            rentData.setSource("slando");
+//            rentData.setAddress("Adress");
+//            rentData.setText("MEssage");
+//            rentData.setPrice("price");
+//            rentData.setDate("Data");
+//            rentData.setUrl("test url");
+//            rentData.setOther("test field other 999");
+//            list.add(rentData);
+
+            RentDataTO rentObj;
+
+            for (int rownum = 0; rownum < list.size(); rownum++) {
+                Row row = sheet.createRow(rownum + lastrow);
+                rentObj = list.get(rownum);
+
+                row.createCell(0).setCellValue(rentObj.getSource());
+                row.createCell(1).setCellValue(rentObj.getAddress());
+                row.createCell(2).setCellValue(rentObj.getText());
+                row.createCell(3).setCellValue(rentObj.getPrice());
+                row.createCell(4).setCellValue(rentObj.getDate());
+                row.createCell(5).setCellValue(rentObj.getUrl());
+                row.createCell(6).setCellValue(rentObj.getOther());
+            }
+
+            file.close();
+            //Write the workbook in file system
+            FileOutputStream out = new FileOutputStream(new File("src/test/resources/rent_list.xlsx"));
+            workbook.write(out);
+            out.close();
+            System.out.println("rent_list.xlsx written successfully on disk.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
     private static void print(String msg, Object... args) {
