a049762fadb6c3071397f7f5df1205677e66fffb
dstoianov <denys.stoianov@gmail.com>
2014-01-21 01:16:48 +0200

    antenka test begin, added some code

--- src/test/java/ua/com/antenka/test/BuyTest.java
+++ src/test/java/ua/com/antenka/test/BuyTest.java
@@ -5,6 +5,9 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -38,6 +41,36 @@ public class BuyTest {
         driver.findElement(By.xpath(".//*[@id='enterCart']")).click();
         driver.findElement(By.xpath(".//*[@id='applyCart']")).click();
         ////*[@id='enterCart']
+
+        driver.findElement(By.id("last_nameClient")).sendKeys("wwrwr");
+        driver.findElement(By.id("nameClient")).sendKeys("wwrwr");
+        driver.findElement(By.id("patronymic")).sendKeys("wwrwr");
+
+        WebElement phone = driver.findElement(By.id("telClient"));
+        phone.clear();
+        phone.sendKeys("+38(303)734-63-74");
+
+
+
+        Select mailService = new Select(driver.findElement(By.id("courier")));
+        List<WebElement> options = mailService.getOptions();
+        mailService.selectByIndex(getRnd(options.size()));
+
+        Select paymentMethods = new Select(driver.findElement(By.id("money")));
+        //List<WebElement> options = mailService.getOptions();
+        paymentMethods.selectByIndex(getRnd(2));
+
+
+
+        Select city = new Select(driver.findElement(By.id("sity")));
+        List<WebElement> cityCount = city.getOptions();
+        city.selectByIndex(getRnd(cityCount.size()));
+
+
+        //print("All count of options is %s ", options.size());
+
+
+
     }
 
     private boolean isElementExist(String xpath) {
@@ -48,6 +81,16 @@ public class BuyTest {
             return false;
         }
     }
+
+    private WebElement findElement(final By by) {
+        return (new WebDriverWait(driver, 10)).until(new ExpectedCondition<WebElement>() {
+            @Override
+            public WebElement apply(WebDriver d) {
+                return d.findElement(by);
+            }
+        });
+    }
+
 
     private int getRnd(int i) {
         Random rand = new Random();
