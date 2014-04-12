82c3ea3c2a8623a1a22aa126572b2d9fa633d43d
dstoianov <denys.stoianov@gmail.com>
2014-04-12 14:38:29 +0300

    refactoring, extract BaseTest for all tests

---
+++ src/test/java/parametrized/runsuite/BaseTest.java
@@ -0,0 +1,58 @@
+package parametrized.runsuite;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.*;
+
+/**
+ * Created by Funker on 12.04.14.
+ */
+public class BaseTest {
+    protected WebDriver driver;
+    private String url;
+
+
+    @BeforeClass
+    @Parameters(value={"browser","version", "url"})
+    public void setUp(@Optional("IE") String browser,
+                      @Optional("9")String version,
+//                      @Optional("WIN") String platform,
+                      @Optional("http://yandex.ru") String url ) throws Exception {
+
+        this.url = url;
+
+        WebDriverFactory instanceDriver = new WebDriverFactory();
+
+        driver = instanceDriver.getDriver(browser, version);
+
+        //driver = instanceDriver.getDriver(browser, version);
+        //driver = instanceDriver.getLocalDriver(browser, version);
+//        driver = Browser2.getBrowser(browser, version).getDriver();
+
+        //driver = WebDriverFactory.getDriver(browser, version, platform);
+        //driver = WebDriverFactory.getDriver(browser, version, platform);
+
+
+    }
+
+
+    @BeforeMethod
+    public void openMainPage (){
+        driver.get(url);
+    }
+
+    @AfterClass
+    public void tearDown()  {
+        driver.quit();
+    }
+
+    public void getBrowserName(){
+        Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
+        String browsername = cap.getBrowserName();
+        String browserversion = cap.getVersion();
+        String url = driver.getCurrentUrl();
+        System.out.println("Name: " + browsername + ", ver: " + browserversion + ", URL: " + url);
+    }
+
+}
--- src/test/java/parametrized/runsuite/TestCase.java
+++ src/test/java/parametrized/runsuite/TestCase.java
@@ -16,38 +16,17 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.Color;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 /**
  * User: stoianod
  * Date: 4/8/14
  */
-public class TestCase {
-
-    private WebDriver driver;
-
-
-    @BeforeClass
-    @org.testng.annotations.Parameters(value={"browser","version","platform"})
-    public void setUp(@Optional("firefox") String browser,
-                      @Optional("33")String version,
-                      @Optional("WIN") String platform) throws Exception {
-        DesiredCapabilities capability = new DesiredCapabilities();
-        //capability.setCapability("platform",platform);
-        capability.setCapability("browserName", browser);
-        //capability.setCapability("browserVersion", version);
-        //capability.setCapability("project", "P1");
-        //capability.setCapability("build", "1.0");
-        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
-    }
+public class TestCase extends BaseTest{
 
     @Test
-    public void testSimple() throws Exception {
+    public void testPositiveSubmit() throws Exception {
         driver.get("http://www.google.com");
-        //driver.get("http://www.yandex.ru");
         getBrowserName();
         System.out.println("Page title is: " + driver.getTitle());
         Assert.assertEquals(driver.getTitle(), "Google");
@@ -61,28 +40,11 @@ public class TestCase {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        WebElement titleElement = driver.findElement(By.className("title"));
-        System.out.println("Padding-bottom: " + titleElement.getCssValue("padding-bottom"));
-        String titleColor = titleElement.getCssValue("color");
-        System.out.println("Title color: " + titleColor);
-
-        Color color = Color.fromString(titleColor);
-        System.out.println(color.asHex());
-        System.out.println(color.asRgb());
-        System.out.println(color.asRgba());
     }
 
-    @AfterClass
-    public void tearDown()  {
-        driver.quit();
-    }
-
-    public void getBrowserName(){
-        Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
-        String browsername = cap.getBrowserName();
-        String browserversion = cap.getVersion();
-        String url = driver.getCurrentUrl();
-        System.out.println("Name: " + browsername + ", ver: " + browserversion + ", URL: " + url);
+    @Test(description =  "Fake Test")
+    public void testRequiredFields()throws Exception{
+        Assert.assertTrue(true);
     }
 
 }
