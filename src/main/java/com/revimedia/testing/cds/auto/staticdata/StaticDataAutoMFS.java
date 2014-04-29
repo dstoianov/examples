21608e72da4569d4159a2936dd3bb1583218cbd6
dstoianov <denys.stoianov@gmail.com>
2014-04-30 01:02:35 +0300

    implemented static data auto mf class

--- src/main/java/com/revimedia/testing/cds/auto/staticdata/StaticDataAutoMFS.java
+++ src/main/java/com/revimedia/testing/cds/auto/staticdata/StaticDataAutoMFS.java
@@ -1,6 +1,6 @@
 package com.revimedia.testing.cds.auto.staticdata;
 
-import java.util.Random;
+import java.util.*;
 
 /**
  * Created by dstoianov on 4/29/2014, 7:31 PM.
@@ -8,13 +8,13 @@ import java.util.Random;
 public class StaticDataAutoMFS {
 
     public StaticDataAutoMFS() {
-        this.insuranceCompany = getRandomFromList(insuranceCompanyList);
-        this.insuredSince = getRandomFromList(insuredSinceList);
-        this.residenceType = getRandomFromList(residenceTypeList);
-        this.maritalStatus = getRandomFromList(maritalStatusList);
-        this.creditRating = getRandomFromList(creditRatingList);
-        this.education = getRandomFromList(educationList);
-        createCar();
+        this.insuranceCompany = getRandomValueFromList(insuranceCompanyList);
+        this.insuredSince = getRandomValueFromList(insuredSinceList);
+        this.residenceType = getRandomValueFromList(residenceTypeList);
+        this.maritalStatus = getRandomValueFromList(maritalStatusList);
+        this.creditRating = getRandomValueFromList(creditRatingList);
+        this.education = getRandomValueFromList(educationList);
+        setRandomCar();
     }
 
     public String getInsuranceCompany() {
@@ -73,13 +73,22 @@ public class StaticDataAutoMFS {
     private String[] creditRatingList = {"Excellent", "Good", "Some Problems", "Major Problems"};
     private String[] educationList = {"Associate Degree", "Bachelors Degree", "Doctorate Degree", "High school diploma", "Masters Degree", "None", "Other", "Some College"};
 
+    private void setRandomCar() {
+        Map<Integer, String[]> car = new HashMap<Integer, String[]>();
+        car.put(0, new String[]{"2003", "FORD", "Focus LX"});
+        car.put(1, new String[]{"1993", "SATURN", "S-Series SW1"});
+        car.put(2, new String[]{"1983", "VOLVO", "240 GLT Turbo"});
+        car.put(3, new String[]{"2012", "AUDI", "TT RS 2-5 quattro"});
+        car.put(4, new String[]{"2009", "BMW", "M3"});
+        car.put(5, new String[]{"2002", "BUICK", "Regal LS"});
 
-    private void createCar() {
-
+        String[] strings = car.get((new Random()).nextInt(car.size()));
+        this.year = strings[0];
+        this.make = strings[1];
+        this.model = strings[2];
     }
 
-
-    private String getRandomFromList(String[] list) {
+    private String getRandomValueFromList(String[] list) {
         Random r = new Random();
         return list[r.nextInt(list.length)];
     }
