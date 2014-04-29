7070084346e462ff5f363238af22e7b69be3809a
dstoianov <denys.stoianov@gmail.com>
2014-04-29 22:33:31 +0300

    refactoring some code

--- src/main/java/com/revimedia/testing/cds/auto/staticdata/StaticDataAutoMFS.java
+++ src/main/java/com/revimedia/testing/cds/auto/staticdata/StaticDataAutoMFS.java
@@ -7,6 +7,52 @@ import java.util.Random;
  */
 public class StaticDataAutoMFS {
 
+    public StaticDataAutoMFS() {
+        this.insuranceCompany = getRandomFromList(insuranceCompanyList);
+        this.insuredSince = getRandomFromList(insuredSinceList);
+        this.residenceType = getRandomFromList(residenceTypeList);
+        this.maritalStatus = getRandomFromList(maritalStatusList);
+        this.creditRating = getRandomFromList(creditRatingList);
+        this.education = getRandomFromList(educationList);
+        createCar();
+    }
+
+    public String getInsuranceCompany() {
+        return insuranceCompany;
+    }
+
+    public String getInsuredSince() {
+        return insuredSince;
+    }
+
+    public String getResidenceType() {
+        return residenceType;
+    }
+
+    public String getMaritalStatus() {
+        return maritalStatus;
+    }
+
+    public String getCreditRating() {
+        return creditRating;
+    }
+
+    public String getEducation() {
+        return education;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
     private String insuranceCompany;
     private String insuredSince;
     private String residenceType;
@@ -26,6 +72,11 @@ public class StaticDataAutoMFS {
     private String[] maritalStatusList = {"Divorced", "Married", "Separated", "Single", "Widowed"};
     private String[] creditRatingList = {"Excellent", "Good", "Some Problems", "Major Problems"};
     private String[] educationList = {"Associate Degree", "Bachelors Degree", "Doctorate Degree", "High school diploma", "Masters Degree", "None", "Other", "Some College"};
+
+
+    private void createCar() {
+
+    }
 
 
     private String getRandomFromList(String[] list) {
