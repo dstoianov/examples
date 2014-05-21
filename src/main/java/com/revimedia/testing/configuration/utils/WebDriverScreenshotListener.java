294dd405502916d89380a3b58853f7eb7f37b3ce
dstoianov <stde@ciklum.com>
2014-05-21 11:58:20 +0300

    qqqq qqq

--- src/main/java/com/revimedia/testing/configuration/utils/WebDriverScreenshotListener.java
+++ src/main/java/com/revimedia/testing/configuration/utils/WebDriverScreenshotListener.java
@@ -42,7 +42,7 @@ public class WebDriverScreenshotListener implements ITestListener {
             //Reporter.setEscapeHtml(false);
             // Reporter.log("<br>V3-1 Saved <a href=\"file:///" + s + "\">Screenshot</a>");
             //Reporter.log("<br>V3-2 Saved <a href=\"../screenshots/" + outFile.getName() + "\">Screenshot</a>");
-            //Reporter.log("<br>V4 Saved <a href=\"screenshots/" + outFile.getName() + "\">Screenshot</a>");
+            Reporter.log("<br>V4 Saved <a href=\"screenshots/" + outFile.getName() + "\">Screenshot</a>");
             Reporter.log("<br>V5 Saved <a href=\"~/screenshots/" + outFile.getName() + "\">Screenshot</a>");
 
 
