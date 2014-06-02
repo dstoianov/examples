aad97943b6317079efa1e6d88afb9a908666f3d4
dstoianov <denys.stoianov@gmail.com>
2014-06-03 01:22:38 +0300

    qqq

--- src/main/java/com/revimedia/testing/configuration/listeners/ScreenshotListener.java
+++ src/main/java/com/revimedia/testing/configuration/listeners/ScreenshotListener.java
@@ -1,4 +1,4 @@
-package com.revimedia.testing.configuration.utils;
+package com.revimedia.testing.configuration.listeners;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
