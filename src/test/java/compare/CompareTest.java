66bf758f108bcedc0a51afbe73aa6ccb631fc6a3
dstoianov <denys.stoianov@gmail.com>
2014-11-04 09:46:37 +0200

    work with Time and Date

--- src/test/java/compare/CompareTest.java
+++ src/test/java/compare/CompareTest.java
@@ -2,6 +2,11 @@ package compare;
 
 import org.testng.annotations.Test;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 /**
  * Created by dstoianov on 6/6/2014, 3:29 PM.
  */
@@ -39,5 +44,20 @@ public class CompareTest {
         System.out.println(equals);
         System.out.println(equals2);
 
+    }
+
+
+    @Test
+    public void testName3() throws Exception {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        String dateInString = "10-08-1982 10:20:56";
+        Date d = sdf.parse(dateInString);
+
+//        Date d = new Date();
+        Calendar cal = new GregorianCalendar();
+        Calendar cal2 = Calendar.getInstance();
+        cal.setTime(d);
+        cal.add(Calendar.DAY_OF_MONTH, 10);
+        Date time = cal.getTime();
     }
 }
