0d545070b9d54a52445cc378974c3a4df7cd45cf
dstoianov <denys.stoianov@gmail.com>
2015-01-09 16:00:04 +0200

    modify auto download files for Chrome and FF

--- src/test/java/tipsandtricks/autoDownloadFile/ForFireFoxTest.java
+++ src/test/java/tipsandtricks/autoDownloadFile/ForFireFoxTest.java
@@ -1,32 +1,36 @@
-package tipsandtricks.ffdefaultdir;
+package tipsandtricks.autoDownloadFile;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
 
 /**
  * Created by Funker on 20.10.2014.
  */
-public class DefaultDownloadDirectoryTest {
+public class ForFireFoxTest {
 
     protected WebDriver driver;
     private String DOWNLOAD_DIR = "C:\\tmp\\selenium";
 
-    //    @BeforeClass
+    private static void creteDirIfNotExist(String path) {
+        File dir = new File(path);
+        if (dir.exists()) {
+            System.out.println("Dir is already exist in the path: " + path);
+        } else {
+            dir.mkdirs();
+            System.out.println("Dir created: " + path);
+        }
+    }
+
+    @BeforeClass
     public void setUp() throws IOException {
         FirefoxProfile profile = new FirefoxProfile();
 
@@ -68,7 +72,6 @@ public class DefaultDownloadDirectoryTest {
         element.click();
     }
 
-
     @Test
     public void testZip() throws Exception {
         driver.get("http://getbootstrap.com/getting-started/");
@@ -81,52 +84,4 @@ public class DefaultDownloadDirectoryTest {
         driver.quit();
     }
 
-    private static void creteDirIfNotExist(String path) {
-        File dir = new File(path);
-        if (dir.exists()) {
-            System.out.println("Dir is already exist in the path: " + path);
-        } else {
-            dir.mkdirs();
-            System.out.println("Dir created: " + path);
-        }
-    }
-
-
-    public WebDriver createChrome() {
-
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-        capabilities.setCapability("chrome.switches", Arrays.asList("--start-maximized"));
-        //http://peter.sh/experiments/chromium-command-line-switches/
-        ///https://sites.google.com/a/chromium.org/chromedriver/capabilities
-
-        //http://stackoverflow.com/questions/25682766/how-to-handle-downloading-popup-alert-while-downloading-a-file-in-chrome
-        //http://stackoverflow.com/questions/23530399/chrome-web-driver-download-files
-
-        return new ChromeDriver(capabilities);
-    }
-
-
-    @BeforeClass
-    public void getChrome2() {
-
-        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
-        chromePrefs.put("profile.default_content_settings.popups", 0);
-        chromePrefs.put("download.default_directory", DOWNLOAD_DIR);
-
-        ChromeOptions options = new ChromeOptions();
-
-        HashMap<String, Object> chromeOptionsMap = new HashMap<String, Object>();
-
-        options.setExperimentalOption("prefs", chromePrefs);
-//        options.addArguments("--test-type");
-//        options.addArguments("start-maximized");
-
-        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
-
-        capabilities.setCapability(ChromeOptions.CAPABILITY, chromeOptionsMap);
-        capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-        capabilities.setCapability(ChromeOptions.CAPABILITY, options);
-
-        driver = new ChromeDriver(capabilities);
-    }
 }
