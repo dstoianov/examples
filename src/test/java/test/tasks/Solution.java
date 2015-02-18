6ffa8bd0e8227500215dec54af580b08fea5e045
dstoianov <denys.stoianov@gmail.com>
2015-02-19 01:02:55 +0200

    test commit

---
+++ src/test/java/test/tasks/Solution.java
@@ -0,0 +1,17 @@
+package test.tasks;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int t = 0; t < n; t++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
