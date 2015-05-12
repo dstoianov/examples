e125dcee13b7fca45bc1c40a3e76fd0704664425
dstoianov <denys.stoianov@gmail.com>
2015-05-12 10:49:17 +0300

    fix pom.xml and add some examples

---
+++ src/test/java/BrowserMob/confetqa2013/Sample5_ChangingLanguage.java
@@ -0,0 +1,48 @@
+package BrowserMob.confetqa2013;
+
+import net.lightbody.bmp.core.har.Har;
+import net.lightbody.bmp.proxy.ProxyServer;
+import net.lightbody.bmp.proxy.http.BrowserMobHttpRequest;
+import net.lightbody.bmp.proxy.http.RequestInterceptor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public class Sample5_ChangingLanguage {
+
+    @Test
+    public void changingLanguage() throws Exception {
+        ProxyServer bmp = new ProxyServer(8071);
+        bmp.start();
+
+        RequestInterceptor languageChanger = new LanguageChanger("en,ru");
+        bmp.addRequestInterceptor(languageChanger);
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(CapabilityType.PROXY, bmp.seleniumProxy());
+
+        WebDriver driver = new FirefoxDriver(caps);
+
+        driver.get("http://ci.seleniumhq.org:8080/");
+        Thread.sleep(10000);
+
+        driver.quit();
+
+        bmp.stop();
+    }
+
+    public static class LanguageChanger implements RequestInterceptor {
+        private String lang;
+
+        public LanguageChanger(String lang) {
+            this.lang = lang;
+        }
+
+        @Override
+        public void process(BrowserMobHttpRequest request, Har har) {
+            request.addRequestHeader("Accept-Language", lang);
+        }
+    }
+}
