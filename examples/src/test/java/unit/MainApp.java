50c1f558eb8181f934634e9d82736dbb2c80312b
Denys Stoianov <dstoianov@plslogistics.com>
2015-08-06 15:06:24 +0300

    add test questions

---
+++ examples/src/test/java/unit/MainApp.java
@@ -0,0 +1,16 @@
+package unit;
+
+/**
+ * Created by dstoianov on 2015-07-31.
+ */
+public class MainApp {
+
+    public static void main(final String[] argv) {
+
+        String s = "ddd";
+        System.out.println(s);
+
+    }
+
+
+}
