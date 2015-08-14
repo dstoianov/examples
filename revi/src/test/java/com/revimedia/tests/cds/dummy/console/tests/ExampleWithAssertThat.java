0c67a152e43ae19b099f47d98dcc32fe385164fa
dstoianov <denys.stoianov@gmail.com>
2015-08-14 23:15:18 +0300

    add some test

--- revi/src/test/java/com/revimedia/tests/cds/dummy/console/tests/ExampleWithAssertThat.java
+++ revi/src/test/java/com/revimedia/tests/cds/dummy/console/tests/ExampleWithAssertThat.java
@@ -34,4 +34,13 @@ public class ExampleWithAssertThat {
         assertThat("yy", is(not("xx")));
         assertThat("i like che_ese", containsString("cheese"));
     }
+
+    @Test
+    public void checkParametrizedBuildTest() {
+        System.out.println("webdriver.chrome.driver path " + System.getProperty("webdriver.chrome.driver"));
+        System.out.println("browser " + System.getProperty("browser"));
+        System.out.println("Guid " + System.getProperty("guid"));
+        System.out.println("Threads count " + System.getProperty("threads"));
+        System.out.println("Note message " + System.getProperty("note"));
+    }
 }
