bf3a1b86354715e6fd333191c7f81d806ea4f3a6
dstoianov <denys.stoianov@gmail.com>
2015-08-15 00:34:55 +0300

    minor changes for test

--- revi/src/test/java/com/revimedia/tests/cds/dummy/console/tests/ExampleWithAssertThat.java
+++ revi/src/test/java/com/revimedia/tests/cds/dummy/console/tests/ExampleWithAssertThat.java
@@ -2,6 +2,11 @@ package com.revimedia.tests.cds.dummy.console.tests;
 
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -39,8 +44,24 @@ public class ExampleWithAssertThat {
     public void checkParametrizedBuildTest() {
         System.out.println("webdriver.chrome.driver path " + System.getProperty("webdriver.chrome.driver"));
         System.out.println("browser " + System.getProperty("browser"));
+//        System.setProperty("guid", "test");
         System.out.println("Guid " + System.getProperty("guid"));
         System.out.println("Threads count " + System.getProperty("threads"));
-        System.out.println("Note message " + System.getProperty("note"));
+        System.out.println("Note message " + System.getProperty("note") + "\n");
+
+        Properties props = System.getProperties();
+        List<String> result = new ArrayList<String>();
+
+        for (String prop : props.stringPropertyNames()) {
+            if (prop.equals("java.class.path") || prop.equals("java.library.path")) {
+                continue;
+            }
+            result.add(String.format("%s = %s", prop, System.getProperty(prop)));
+        }
+        Collections.sort(result);
+        int i = 0;
+        for (String s : result) {
+            System.out.println(++i + ") " + s);
+        }
     }
 }
