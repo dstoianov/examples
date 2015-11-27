872be0838e6b5eb182f19475a16dbf255e6f6c1b
Denys Stoianov <dstoianov@plslogistics.com>
2015-11-27 20:14:54 +0200

    add example  CommonElementsInArray

---
+++ examples/src/test/java/unit/FindCommonElementsInArray.java
@@ -0,0 +1,24 @@
+package unit;
+
+/**
+ * Created by dstoianov on 2015-11-27.
+ */
+public class FindCommonElementsInArray {
+
+    public static void main(String a[]) {
+
+        int[] arr1 = {4, 7, 3, 9, 2};
+        int[] arr2 = {3, 2, 12, 9, 40, 32, 4};
+
+        for (int i = 0; i < arr1.length; i++) {
+
+            for (int j = 0; j < arr2.length; j++) {
+
+                if (arr1[i] == arr2[j]) {
+
+                    System.out.println(arr1[i]);
+                }
+            }
+        }
+    }
+}
