a2c0dea7598260316bce0f0ac1d74a3f57d905fd
dstoianov <denys.stoianov@gmail.com>
2015-12-29 23:13:41 +0200

    inject jQuery lib to page

--- tips-and-tricks/src/test/java/com/tipsandtricks/jquery/inject/InjectJQueryTest.java
+++ tips-and-tricks/src/test/java/com/tipsandtricks/jquery/inject/InjectJQueryTest.java
@@ -3,10 +3,17 @@ package com.tipsandtricks.jquery.inject;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * An example of loading jQuery dynamically using WebDriver.
@@ -17,12 +24,22 @@ public class InjectJQueryTest {
 //    private static final String JQUERY_LOAD_SCRIPT = "resources\\jQuerify.js";
     private static final String JQUERY_LOAD_SCRIPT = "src/test/resources/jquery-1.7.2.js";
 
-    // driver
-    public static void main(String[] args) throws Exception {
+    public WebDriver driver;
 
-        WebDriver driver = new FirefoxDriver();
+    @BeforeMethod
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        if (driver != null)
+            driver.quit();
+    }
+
+    @Test
+    public void simpleTest() throws IOException {
         driver.get("http://www.google.com");
-
         String jQueryLoader = readFile(JQUERY_LOAD_SCRIPT);
 
         // give jQuery time to load asynchronously
@@ -37,7 +54,6 @@ public class InjectJQueryTest {
                         " }); "
         );
 
-        driver.quit();
     }
 
     // helper method
@@ -56,5 +72,32 @@ public class InjectJQueryTest {
         } finally {
             stream.close();
         }
+    }
+
+
+    @Test
+    public void injectjQueryIntoGoogle() throws Exception {
+        driver.get("http://www.google.com");
+        assertThat(isjQueryLoaded(), is(equalTo(false)));
+        injectScript("https://code.jquery.com/jquery-latest.min.js");
+        assertThat(isjQueryLoaded(), is(equalTo(true)));
+    }
+
+    public Boolean isjQueryLoaded() throws Exception {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        return (Boolean) js.executeScript("return typeof jQuery != 'undefined';");
+    }
+
+    public void injectScript(String scriptURL) throws Exception {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("function injectScript(url) {\n" +
+                " var script = document.createElement ('script');\n" +
+                " script.src = url;\n" +
+                " var head = document.getElementsByTagName('head')[0];\n" +
+                " head.appendChild(script);\n" +
+                "}\n" +
+                "\n" +
+                "var scriptURL = arguments[0];\n" +
+                "injectScript(scriptURL);", scriptURL);
     }
 }
